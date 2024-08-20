0e3e8839ec33f8621719e7b63e9aaea44aa03d96
sergeytsvir <tsvir36@gmail.com>
2024-08-20 14:17:06 +0300

    feat: solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj = {
+    '(':')',
+    '[':']',
+    '{':'}',
+    '|':'|',
+    '1':'2',
+    '3':'4',
+    '5':'6',
+    '7':'7',
+    '8':'8'
+  }
+  let arr = [];
+  for(let i = 0; i < str.length; i++){
+    if(arr.length === 0) arr.push(str[i])
+    else if (obj[arr[arr.length -1]] === str[i]) arr.pop()
+    else if(arr[arr.length - 1] === str[i] || arr[arr.length - 1] !== str[i]) arr.push(str[i])
+  }
+  return arr.length === 0
 }
